f41270624737f239445577632099053ef700f714
Elias Eskelinen <104145838+EskelinenElias@users.noreply.github.com>
2025-01-27 15:02:21 +0200

    Task 5, added pagination

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,6 +11,7 @@ function About() {
   
   // State for storing data
   const [data, setData] = useState<Post[]>([]);
+  const [numVisibleItems, setNumVisibleItems] = useState(12);
 
   // Function for fetching data
   useEffect(() => {
@@ -19,13 +20,18 @@ function About() {
       .then(response => response.json())
       .then((data: Post[]) => setData(data));
   }, []);
+  
+  // Function to show more items
+  function showMore() {
+    setNumVisibleItems((previousNumVisibleItems) => previousNumVisibleItems + 12);
+  }
 
   // Component
   return (
     <div>
       <div className="grid-container">
         {
-          data.map((item) => {
+          data.slice(0, numVisibleItems).map((item) => {
             return (
               <div className="grid-item" key={item.id}>
                 <h3>{item.title}</h3>
@@ -35,6 +41,9 @@ function About() {
           })
         }
       </div>
+      {numVisibleItems < data.length && (
+        <button onClick={showMore} className="show-more-button">Show More</button>
+      )}
       <p>This is About</p>
     </div>
   );
