0c4e54554d2482bdf7c4d58c931dc1538e292e5b
Elias Eskelinen <104145838+EskelinenElias@users.noreply.github.com>
2025-01-27 13:36:56 +0200

    Task 2

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,31 @@
+import { useState, useEffect } from "react"; 
+
 function About() {
+  
+  // State for storing data
+  const [data, setData] = useState([]);
+
+  // Function for fetching data
+  useEffect(() => {
+    const address = 'https://jsonplaceholder.typicode.com/posts'
+    fetch(address)
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
+  // Component
   return (
     <div>
+      {
+        data.map((item) => {
+          return (
+            <div key={item.id}>
+              <h3>{item.title}</h3>
+              <p>{item.body}</p>
+            </div>
+          );
+        })
+      }
       <p>This is About</p>
     </div>
   );
