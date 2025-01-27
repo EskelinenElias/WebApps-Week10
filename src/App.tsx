13a86428a1e6be96699904d6849288b251dfda0c
Elias Eskelinen <104145838+EskelinenElias@users.noreply.github.com>
2025-01-27 14:22:42 +0200

    Task 3, internationalization

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from 'react';
 import './App.css'
 import MyContainer from './components/MyContainer'; 
 import Header from './components/Header';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import About from './components/About';
+import { useTranslation } from 'react-i18next';
 
-function App() {
+function Router() {
+  const { t, i18n } = useTranslation();
   return (
     <div className="App">
       <BrowserRouter>
@@ -13,6 +16,7 @@ function App() {
           // Home page
           <Route path="/" element={
             <>
+              <h1>{t('frontPage')}</h1>
               <MyContainer/>
             </>
           }/>
@@ -28,4 +32,13 @@ function App() {
   )
 }
 
-export default App
+function App() {
+  return (
+    <Suspense fallback="loading">
+      <Router/>
+    </Suspense>
+  );
+
+}
+
+export default App; 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,23 @@
 import "../styles/header.css";
 import { Link } from "react-router-dom"; 
+import { useTranslation } from 'react-i18next';
 
 function Header() {
+  const { t, i18n } = useTranslation();
+
+  // Function to change the language
+  function changeLanguage(language) {
+    i18n.changeLanguage(language);
+  };
+  
   return (
     <header>
       <h1>Advanced Web App</h1>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <button>FI</button>
-        <button>EN</button>
+        <Link to="/">{t('home')}</Link>
+        <Link to="/about">{t('about')}</Link>
+        <button id="fi" onClick={() => changeLanguage('fi')}>FI</button>
+        <button id="en" onClick={() => changeLanguage('en')}>EN</button>
       </nav>
     </header>
   );
